b9bcc3ab645337ae382b291708ef5f9941965bc0
shilpafinu <shilpaalex92@gmail.com>
2022-01-26 01:34:52 +0530

    company name search in admin interface project list

--- projement/projects/admin.py
+++ projement/projects/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from projects.models import Company, Project
+#company name filter
+class CompanyFilter(admin.SimpleListFilter):
+    title = _('Company')
+    parameter_name = 'company'
 
+    def lookups(self, request, model_admin):
+        companies = []
+        qs = Company.objects.filter(id__in = model_admin.model.objects.all().values_list('company_id', flat = True).distinct())
+        for c in qs:
+            companies.append([c.id, c.name])
+        return companies
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(company__id__exact=self.value())
+        else:
+            return queryset
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'company', 'start_date', 'end_date')
-    list_filter = ('company__name',)
+    list_filter = (CompanyFilter,)
     ordering = ('-start_date',)
 
     fieldsets = (
